6d7e09b453be99c9622d1905601f35407d30666a
thghost21 <tylerhacker21@gmail.com>
2025-02-25 14:33:15 -0700

    draw snacks

--- app/AppState.js
+++ app/AppState.js
@@ -8,7 +8,11 @@ class ObservableAppState extends EventEmitter {
   examples = []
 
   snacks = [
-    new Snack({ snackName: 'candyBar', snackPrice: 1, snackPic: 'https://images.unsplash.com/photo-1626697556651-67ebdcb8cbd6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNob2NvbGF0ZSUyMGJhcnxlbnwwfHwwfHx8Mg%3D%3D' })
+    new Snack({ snackName: 'candyBar', snackPrice: 2.00, snackPic: 'https://images.unsplash.com/photo-1626697556651-67ebdcb8cbd6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNob2NvbGF0ZSUyMGJhcnxlbnwwfHwwfHx8Mg%3D%3D' }),
+    new Snack({ snackName: 'lollypop', snackPrice: 1.00, snackPic: 'https://images.unsplash.com/photo-1626697556651-67ebdcb8cbd6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNob2NvbGF0ZSUyMGJhcnxlbnwwfHwwfHx8Mg%3D%3D' }),
+    new Snack({ snackName: 'chips', snackPrice: 2.50, snackPic: 'https://images.unsplash.com/photo-1626697556651-67ebdcb8cbd6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNob2NvbGF0ZSUyMGJhcnxlbnwwfHwwfHx8Mg%3D%3D' }),
+    new Snack({ snackName: 'sportsDrink', snackPrice: 2.00, snackPic: 'https://images.unsplash.com/photo-1626697556651-67ebdcb8cbd6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNob2NvbGF0ZSUyMGJhcnxlbnwwfHwwfHx8Mg%3D%3D' }),
+    new Snack({ snackName: 'Soda', snackPrice: 3.00, snackPic: 'https://images.unsplash.com/photo-1626697556651-67ebdcb8cbd6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNob2NvbGF0ZSUyMGJhcnxlbnwwfHwwfHx8Mg%3D%3D' }),
   ]
 }
 
--- app/controllers/SnacksController.js
+++ app/controllers/SnacksController.js
@@ -1,9 +1,19 @@
+import { AppState } from "../AppState.js"
+
 export class SnacksController {
   constructor() {
-
+    console.log('Snacks Controller Working')
+    this.drawSnacksCards()
   }
 
   drawSnacksCards() {
+    const snacks = AppState.snacks
+    let snackListContent = ''
+    snacks.forEach(snack => snackListContent += snack.snacksHTMLTemplate)
+
+
+    const snackListElem = document.getElementById('snackBar')
+    snackListElem.innerHTML = snackListContent
 
   }
 }
